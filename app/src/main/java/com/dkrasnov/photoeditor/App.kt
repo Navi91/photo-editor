ff2152da70866079b6d73b7b12bef02f3eb29c1b
dkrasnov <navi91@inbox.ru>
2019-04-14 20:28:04 +0300

    Add toolbar layout.

---
+++ app/src/main/java/com/dkrasnov/photoeditor/App.kt
@@ -0,0 +1,13 @@
+package com.dkrasnov.photoeditor
+
+import android.app.Application
+import android.util.Log
+import java.util.*
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/dkrasnov/photoeditor/glide/PhotoEditorAppGlideModule.kt
@@ -0,0 +1,5 @@
+package com.dkrasnov.photoeditor.glide
+
+import com.bumptech.glide.module.AppGlideModule
+
+class PhotoEditorAppGlideModule: AppGlideModule()
